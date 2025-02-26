35cb3a98b69ec25e7d10021882b1f66744bd2ebd
bret <bretai@LCARS-MX-23>
2025-02-26 10:19:05 +0300

    Set up GraphQL + REST Codegen, and Swagger for REST API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,39 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
+import { JwtModule } from "@nestjs/jwt";
+import { join } from "path";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { PrismaModule } from "./common/prisma/prisma.module";
+import { UsersModule } from "./models/users/users.module";
+
+// Todo: Move to Utils lib
+const MAX_AGE = 24 * 60 * 60;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: MAX_AGE },
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      introspection: true,
+      fieldResolverEnhancers: ["guards"],
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      buildSchemaOptions: {
+        numberScalarMode: "integer",
+      },
+      debug: true,
+      playground: true,
+    }),
+    PrismaModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/common/auth/auth.decorator.ts
@@ -0,0 +1,19 @@
+import {
+  applyDecorators,
+  createParamDecorator,
+  ExecutionContext,
+  SetMetadata,
+  UseGuards,
+} from "@nestjs/common";
+import { Role } from "src/common/types";
+
+import { GqlExecutionContext } from "@nestjs/graphql";
+import { AuthGuard } from "./auth.guard";
+
+export const AllowAuthenticated = (...roles: Role[]) =>
+  applyDecorators(SetMetadata("roles", roles), UseGuards(AuthGuard));
+
+export const GetUser = createParamDecorator((data, ctx: ExecutionContext) => {
+  const context = GqlExecutionContext.create(ctx);
+  return context.getContext().req.user;
+});
